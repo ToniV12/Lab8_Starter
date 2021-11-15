e13b1ad3f2e29b00e357620ba8ded837660bf245
TonyV12 <avillalb@ucsd.edu>
2021-11-14 16:26:35 -0800

    finish explore section

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Anthony Villalba
 ## Check Your Understanding:
 1) for our project i would personally like to have it on a github action and locally on my machine so that i can iron out bugs before they get pushed and for added redundancy the action will check again.
 2) You should not use E2E testing since it would run over the specific function you are testing and continue to run to completion regardless of output.
-3) 
+3) I don think it would be a good option to use unit testing since theres no way to test if the parts are working together, only if the message was sent to the server, but never if it was recieved by the other person or the server.
+4) yes you should use a unit test for that because that is client based and is easy to check using a unit test.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,72 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+//is date tests
+test('true isdate 1',() =>{
+    expect(functions.isDate('02/15/1993')).toBe(true);
+});
+test('true isdate 2',() =>{
+    expect(functions.isDate('06/12/2015')).toBe(true);
+});
+test('false isdate 1',() =>{
+    expect(functions.isDate('02/15/18907')).toBe(false);
+});
+test('false isdate 2',() =>{
+    expect(functions.isDate('223/15/1890')).toBe(false);
+});
+
+//phone number tests
+test('true isphoneNumber 1',() =>{
+    expect(functions.isPhoneNumber('(626) 639-9028')).toBe(true);
+});
+test('true isphoneNumber 2',() =>{
+    expect(functions.isPhoneNumber('686-939-8888')).toBe(true);
+});
+test('false isphoneNumber 1',() =>{
+    expect(functions.isPhoneNumber('996 837 9028')).toBe(false);
+});
+test('false isphoneNumber 2',() =>{
+    expect(functions.isPhoneNumber('999-939-888')).toBe(false);
+});
+
+//email tests
+test('true isEmail 1',() =>{
+    expect(functions.isEmail('123@gmail.com')).toBe(true);
+});
+test('true isEmail 2',() =>{
+    expect(functions.isEmail('123@yahoo.com')).toBe(true);
+});
+test('false isEmail 1',() =>{
+    expect(functions.isEmail('123yahoo.com')).toBe(false);
+});
+test('false isEmail 2',() =>{
+    expect(functions.isEmail('balonieboi**@yahoo.io')).toBe(false);
+});
+
+// is strong password tests
+test('true isStrongPassword 1',() =>{
+    expect(functions.isStrongPassword('Qwht6')).toBe(true);
+});
+test('true isStrongPassword 2',() =>{
+    expect(functions.isStrongPassword('KJHASKddj')).toBe(true);
+});
+test('false isStrongPassword 1',() =>{
+    expect(functions.isStrongPassword('kjdhsaui*asdjksa')).toBe(false);
+});
+test('false isStrongPassword 2',() =>{
+    expect(functions.isStrongPassword('kjdhsauisakdlas;dekuja')).toBe(false);
+});
+
+//is hex color tests
+test('true isHexColor 1',() =>{
+    expect(functions.isHexColor('#A1C')).toBe(true);
+});
+test('true isHexColor 2',() =>{
+    expect(functions.isHexColor('#FFF')).toBe(true);
+});
+test('false isHexColor 1',() =>{
+    expect(functions.isHexColor('R 256, G78, B90')).toBe(false);
+});
+test('false isHexColor 2',() =>{
+    expect(functions.isHexColor('#ADSDJ15')).toBe(false);
+});
